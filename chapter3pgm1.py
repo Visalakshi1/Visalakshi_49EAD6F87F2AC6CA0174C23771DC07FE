7da9404eb245fe262ffae92210c00a57a000cf4d
Visalakshi1 <144351070+Visalakshi1@users.noreply.github.com>
2023-09-23 07:19:29 +0530

    Add files via upload

---
+++ chapter3pgm1.py
@@ -0,0 +1,12 @@
+def linearsearchproduct(productlist,targetproduct):
+  indices=[]
+  for index,product in enumerate(productlist):
+   if product == targetproduct:
+     indices.append(index)
+  return indices
+products= ["shoes","boot","loafter","shoes","sandal","shoes"]
+target ="shoes"
+target2= 'apple'
+result= linearsearchproduct(products,target)
+print(result)
+
